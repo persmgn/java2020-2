e8c48381f1d5746f455bc20bb44dd1372ef6537d
chav1961 <chav1961@winmain>
2020-12-05 11:03:59 +0500

    Daily commit

--- src/lesson1/HelloWorld.java
+++ src/lesson1/HelloWorld.java
@@ -75,7 +75,77 @@ public class HelloWorld /*extends java.lang.Object*/ {
 		float	fc1 = 0.35F, fc2 = 1.25e-10F, fc3 = 12.3_45e1_5F;
 		
 		char	c1 = 10, c2 = '\r', c3 = '\012', c4 = '\uaa22';
-	
+		
+		x4 = (x1 = x2 + x3); 
+		x4 += x5;	// x4 = x4 + x5; 
+
+		if (x1 == 0) {	// begin end
+			x1 *= 2;
+		}
+		else {
+			// ......
+		}
+		
+		while (x2 > 0) {
+			--x2;
+		}
+
+//repeat:		
+		do {
+			//
+			if (/*????*/) {
+				continue; // goto repeat;
+			}
+		} while (x2 > 0);
+		
+		for (int index = 0; index < 10; index++) {
+			// body
+		}
+		
+		int index = 0;
+		while (index < 10) {
+			// body
+			if (/*????*/) {
+				break; // goto exit;
+			}
+			if (/*????*/) {
+				continue; // goto repeat;
+			}
+repeat:		index++;
+		}
+//exit:		
+		// break;
+		// continue;
+		
+exit1:	for (int x = 0; x < 10; x++) {
+			for (int y = 0; y < 10; y++) {
+				if (/*????*/) {
+					break exit1; // goto exit1;
+				}
+			}
+		}
+//exit1:		
+		
+		switch (x1) {
+			case 0 : 
+				// op1
+				break;
+			case 1 :
+				// op2
+				break;
+			case 2 :
+				// op3
+				break;
+			case 1000102 :
+				// op4
+				break;
+			default :
+				// op5
+		}
+
+		return;
+		
+		
 	}
 }
 
